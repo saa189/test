3aafec9154f904739b6c4f7f3a9e09c577e786db
Sergeev <saa189@mail.msiu.ru>
2013-03-25 14:11:58 +0400

    commit02

---
+++ agent.sh
@@ -0,0 +1,3 @@
+SSH_AUTH_SOCK=/tmp/ssh-oiU749meRBFH/agent.17252; export SSH_AUTH_SOCK;
+SSH_AGENT_PID=17253; export SSH_AGENT_PID;
+echo Agent pid 17253;
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diplom::Application.config.secret_token = '43fc86701631034cd231a6ef0e5348548c8c7ee8bf1a250395dd34c388c138306c6f99ca828295e7a64bda56024758539f6d6dd6bbdc3d3b552c5ec385f55321'
